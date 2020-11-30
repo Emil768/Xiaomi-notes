5e4ba5a8d446dbfaca793c7644b560ca2d95d1d6
Your Name <murakhas@list.ru>
2020-11-30 17:09:30 +0300

    fix and responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route } from "react-router";
 import "./App.scss";
-import ModalInfo from "./components/ModalInfo";
+
 import Notes from "./components/Notes";
 
 function App() {
